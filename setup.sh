5994788028bad12497c13ce17c911b5d9de0b589
TECH WIZARDS <33427932+taphix@users.noreply.github.com>
2024-08-28 22:10:50 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-# Обновляем информацию о пакетах
-sudo apt update
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+    VERSION_ID=$VERSION_ID
+else
+    echo "Невозможно определить операционную систему"
+    exit 1
+fi
 
-# Устанавливаем необходимые пакеты
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+echo "Определена операционная система: $OS $VERSION_ID"
 
-# Добавляем ключ GPG для Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+install_docker_debian() {
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt install -y docker-ce
+}
 
-# Добавляем репозиторий Docker
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+install_docker_centos() {
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+}
 
-# Обновляем информацию о пакетах с учетом нового репозитория
-sudo apt update
+case $OS in
+    ubuntu|debian)
+        install_docker_debian
+        ;;
+    centos|rhel)
+        install_docker_centos
+        ;;
+    *)
+        echo "Этот скрипт не поддерживает установку Docker на $OS."
+        exit 1
+        ;;
+esac
 
-# Устанавливаем Docker
-sudo apt install -y docker-ce
-
-# Проверяем статус Docker
 sudo systemctl status docker --no-pager
 
-# Если Docker активен, продолжаем установку Docker Compose
 if systemctl is-active --quiet docker; then
     echo "Docker is active, proceeding with Docker Compose installation..."
 
-    # Устанавливаем Docker Compose
     sudo curl -L "https://github.com/docker/compose/releases/download/2.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-    # Делаем Docker Compose исполняемым
     sudo chmod +x /usr/local/bin/docker-compose
 
-    # Проверяем версию Docker Compose
     docker compose version
 
-    # Выводим сообщение об успешной настройке Docker
     echo "Docker настроен успешно"
 else
     echo "Docker не активен, проверьте установку Docker."
